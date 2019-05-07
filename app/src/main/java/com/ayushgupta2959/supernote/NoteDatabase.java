621e91ebb55afbbe9303c2b131afd5ec23c88c3d
ayushgupta2959 <37008291+ayushgupta2959@users.noreply.github.com>
2019-05-07 08:45:51 +0530

    image addition and retrival in database

--- app/src/main/java/com/ayushgupta2959/supernote/NoteDatabase.java
+++ app/src/main/java/com/ayushgupta2959/supernote/NoteDatabase.java
@@ -1,6 +1,9 @@
 package com.ayushgupta2959.supernote;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
@@ -9,14 +12,17 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import java.io.ByteArrayOutputStream;
+
 @Database(entities = {Note.class}, version=1)
 public abstract class NoteDatabase extends RoomDatabase {
 
     private static NoteDatabase instance;
-
+    private static Context myApplicationContext;
     public abstract NoteDao noteDao();
 
     public static synchronized NoteDatabase getInstance(Context context){
+        myApplicationContext = context;
         if(instance==null){
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class,"note_database")
@@ -43,8 +49,13 @@ public abstract class NoteDatabase extends RoomDatabase {
         }
         @Override
         protected Void doInBackground(Void... voids){
-            noteDao.insert(new Note("Title 1","Desc 1",1,null));
-            noteDao.insert(new Note("Title 2","Desc 2",2,null));
+            BitmapDrawable drawable = (BitmapDrawable) myApplicationContext.getResources().getDrawable(R.drawable.b);
+            Bitmap image = drawable.getBitmap();
+            ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+            image.compress(Bitmap.CompressFormat.PNG, 100, bStream);
+            byte[] byteArray = bStream.toByteArray();
+            noteDao.insert(new Note("Title 1","Desc 1",1,byteArray));
+            noteDao.insert(new Note("Title 2","Desc 2",2,byteArray));
             noteDao.insert(new Note("Title 3","Desc 3",3,null));
             return null;
         }
